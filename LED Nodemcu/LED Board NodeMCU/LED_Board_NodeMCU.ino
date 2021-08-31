dc8ee5ebbedcf4229aa78557c95b84482c96aa28
Irshad Faqih <89823781+MRROBO28@users.noreply.github.com>
2021-08-30 21:10:31 -0700

    Add files via upload

---
+++ LED Nodemcu/LED Board NodeMCU/LED_Board_NodeMCU.ino
@@ -0,0 +1,20 @@
+/*=======================================
+ * Board : NodeMCU 
+ * Project : Kontrol Led yang ada di board NodeMCU
+ * Code by : MRROBO28
+ ========================================*/
+
+//===== Variabel LED
+int LED = LED_BUILTIN; // Jika menggunakan led pada board maka menggunakan LED_BUILTIN
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(LED,HIGH);    // ==>LED ON
+  delay(1000);               // ==>Jeda 1 detik
+  digitalWrite(LED,LOW);     // ==>LED OFF
+  delay(1000);
+}
